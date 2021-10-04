636dbbb43db4191ca7826242b4591a1a09c4016e
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-10-04 18:31:39 -0300

    rename the variables

--- mergeSort.c
+++ mergeSort.c
@@ -2,66 +2,76 @@
 Chapter 2.2 - Merge Sort
 Divide and Conquer algorithm
 */
+
 #include <stdio.h>
 #include <math.h>
-#define DIM 8
+#define DIM 9
 
-void merge(int A[], int p, int q, int r) {
-  int n1 = q - p + 1;
-  int n2 = r - q;
-  int L[n1 + 1];
-  int R[n2 + 1];
-  for(int i = 0; i < n1; i++) L[i] = A[p + i];
-  for(int j = 0; j < n2; j++) R[j] = A[q + j + 1];
+void merge(int A[], int begin, int middle, int end) {
+  int n1 = (middle - begin) + 1;
+  int n2 = end - middle;
+  int B[n1 + 1];
+  int C[n2 + 1];
+
+  for(int i = 0; i < n1; i++)
+    B[i] = A[begin + i];
+
+  for(int j = 0; j < n2; j++)
+    C[j] = A[middle + j + 1];
   
   int i = 0;
   int j = 0;
-  int k = p;
+  int k = begin;
 
   while(i < n1 && j < n2) {
-    if(L[i] <= R[j]) {
-      A[k] = L[i];
+    if(B[i] <= C[j]) {
+      A[k] = B[i];
       i++;
     } else {
-      A[k] = R[j];
-      j++; 
+      A[k] = C[j];
+      j++;
     }
     k++;
   }
-
+  
   while(i < n1) {
-    A[k] = L[i];
+    A[k] = B[i];
     i++;
     k++;
   }
 
   while(j < n2) {
-    A[k] = R[j];
+    A[k] = C[j];
     j++;
     k++;
   }
 }
 
-void mergeSort(int A[], int p, int r) {
-  if (p < r) {
-    int q = p + (r - p) / 2;
-    mergeSort(A, p, q);
-    mergeSort(A, q + 1, r);
-    merge(A, p, q, r);
+void mergeSort(int A[], int begin, int end) {
+  if(begin < end) {
+    int middle = begin + (end - begin) / 2; 
+    mergeSort(A, begin, middle);
+    mergeSort(A, middle + 1, end);
+    merge(A, begin, middle, end);
   }
 }
 
-void printArray(int A[], int size) {
+void printArray(int arr[], int size) {
+  printf("[ ");
   for(int i = 0; i < size; i++) {
-    printf("%d ", A[i]);
+    printf("%d ", arr[i]);
   }
-  printf("\n");
+  printf("]\n");
 }
 
 int main() {
-  int A[DIM] = {3, 41, 52, 26, 38, 57, 9, 49};
+  int A[DIM] = {2, 9, 22, 19, 17, 150, 15, 191, 117};
+  
+  printf("Before: ");
   printArray(A, DIM);
+  
   mergeSort(A, 0, DIM - 1);
+
+  printf("After: ");
   printArray(A, DIM);
-  return 0;
 }
