c3f5460d45bc3833399aebe079099d44eec3ba3a
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-10-20 18:34:20 -0300

    add new data structure

---
+++ tree/tree.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+  
+  def __str__(self):
+    return str(self.data)
+
+class BinaryTree:
+  def __init__(self, data = None):
+    if data:
+      node = Node(data)
+      self.root = node
+    else:
+      self.root = None
+
+  def simetric_traversal(self, node = None):
+    if node is None:
+      node = self.root
+    if node.left:
+      self.simetric_traversal(node.left)
+    print(node)
+    if node.right:
+      self.simetric_traversal(node.right)
