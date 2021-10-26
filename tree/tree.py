e1aaee782d4e37edda2a071c4b596546b96890a9
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-10-25 21:07:52 -0300

    add post order and height of binarytree

--- tree/tree.py
+++ tree/tree.py
@@ -6,7 +6,6 @@ class Node:
   
   def __str__(self):
     return str(self.data)
-
 class BinaryTree:
   def __init__(self, data = None):
     if data:
@@ -15,11 +14,33 @@ class BinaryTree:
     else:
       self.root = None
 
-  def simetric_traversal(self, node = None):
+  def inorder_traversal(self, node = None):
     if node is None:
       node = self.root
     if node.left:
-      self.simetric_traversal(node.left)
+      self.inorder_traversal(node.left)
     print(node)
     if node.right:
-      self.simetric_traversal(node.right)
+      self.inorder_traversal(node.right)
+
+  def postorder_traversal(self, node = None):
+    if node is None:
+      node = self.root
+    if node.left:
+      self.postorder_traversal(node.left)
+    if node.right:
+      self.postorder_traversal(node.right)
+    print(node)
+
+  def height(self, node = None):
+    if node is None:
+      node = self.root
+    hleft = 0
+    hright = 0
+    if node.left:
+      self.height(node.left)
+    if node.right:
+      self.height(node.right)
+    if hright > hleft:
+      return hright + 1
+    return hleft + 1
